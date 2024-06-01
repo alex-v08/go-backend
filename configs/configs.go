1ba52970bec0e506863cbb1acc8d6d4acfad7e7c
alex-v08 <alex.velasquez08@outlook.com>
2024-06-01 12:48:12 -0300

    dockerfile

--- configs/configs.go
+++ configs/configs.go
@@ -10,7 +10,7 @@ var DB *gorm.DB
 
 func ConnectToDB() {
 	var err error
-	dsn := "root:changeme@tcp(127.0.0.1:3306)/databasename?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:changeme@tcp(127.0.0.1:3306)/msvcproduct?charset=utf8mb4&parseTime=True&loc=Local"
 	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
 	if err != nil {
